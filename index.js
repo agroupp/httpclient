c32ebbac1e358b558654461713f13a8e57a2da28
Arthur Groupp <arth.groupp@gmail.com>
2019-10-23 13:23:09 +0300

    Fixed exporting error

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 /**
  * 
  */
-exports.HttpContent = require('./lib/http-content');
+exports.HttpContent = require('./lib/http-content').HttpContent;
 
 /**
  * Represents a HTTP response message including the status code and data.
  */
-exports.HttpResponseMessage = require('./lib/http-response-message');
+exports.HttpResponseMessage = require('./lib/http-response-message').HttpResponseMessage;
 
 /**
  * Provides a base class for sending HTTP requests and receiving HTTP responses from a resource identified by a URI.
  */
-exports.HttpClient = require('./lib/http-client');
+exports.HttpClient = require('./lib/http-client').HttpClient;
